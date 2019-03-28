8eb94db604bff6b5d373568314ff6f190317981e
Hoang Nam (particle4dev) <particle4dev@gmail.com>
2019-03-28 11:59:06 +0700

    update bio section

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Fullstack Developer / DevOps / Open source / Scrum Master`,
+    title: `Front End / Fullstack Developer / DevOps / Scrum Master`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@particle4dev`,
   },
